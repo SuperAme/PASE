996648ce31fcf08ea189753fb3be2c883a4bf07b
Americo Meneses <americo.meneses@infotec.mx>
2025-07-04 21:32:00 -0600

    feat: add project

---
+++ PASE/PASEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PASEApp.swift
+//  PASE
+//
+//  Created by Américo MQ on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PASEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
